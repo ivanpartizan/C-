e4aa699021dd064efe74f742d85cd5a36c874aeb
Ivan <argentinaivan@hotmail.com>
2024-06-30 01:16:49 +0200

    Update Fundamentals.cs

--- Fundamentals.cs
+++ Fundamentals.cs
@@ -67,3 +67,39 @@ for (int i = 0; i < 10; i++)
         break;
     }
 }
+
+// Arrays
+int[] numbers = new int[5];
+
+numbers[0] = 4;
+numbers[1] = 8;
+numbers[2] = 15;
+numbers[3] = 16;
+numbers[4] = 23;
+
+Console.WriteLine(numbers[1]);
+Console.WriteLine(numbers.Length);
+
+int[] numbers1 = new int[] { 4, 8, 15, 16, 23, 42 };
+string[] names = new string[] { "John", "Michael", "Bob", "David" };
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+string zig = "You can get what you want out of life " + 
+    "if you help enough other people get what they want.";
+
+char[] charArray = zig.ToCharArray();
+Array.Reverse(charArray);
+
+foreach (char zigChar in charArray)
+{
+    Console.Write(zigChar);
+}
